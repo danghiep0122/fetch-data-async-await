94066958b0d2c2e48db452de64f7642d7a708ea5
Nguyen Dang Hiep <99936776+danghiep0122@users.noreply.github.com>
2022-09-29 16:07:31 +0700

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,15 @@
+useEffect(() => {
+    const url = 'https://api.mockaroo.com/api/26f073c0?count=20&key=0f870f80'
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const json = await response.json()
+        console.log(json)
+        setData(json)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+    fetchData()
+  }, [])
