e62234a45e96497e8cb8be4cfd9232947b3b6f13
Nguyen Dang Hiep <99936776+danghiep0122@users.noreply.github.com>
2022-10-28 12:18:35 +0700

    Create mockData.js

---
+++ mockData.js
@@ -0,0 +1,55 @@
+const allItems = [
+  {
+    id: 1,
+    title: 'Pizza Thập Cẩm Thượng Hạng - Extravaganza',
+    image: 'https://img.dominos.vn/Pizza-Thap-Cam-Thuong-Hang-Extravaganza.jpg',
+    price: '89,000',
+  },
+  {
+    id: 2,
+    title: 'Pizza Bò & Tôm Nướng Kiểu Mỹ - Surf & Turf',
+    image: 'https://img.dominos.vn/Surf-turf-Pizza-Bo-Tom-Nuong-Kieu-My-1.jpg',
+    price: '99,000',
+  },
+  {
+    id: 3,
+    title: 'PIZZA LẠP XƯỞNG XỐT TRỨNG MUỐI MAYONNAISE - SAIGON MANIA PIZZA',
+    image: 'https://img.dominos.vn/pizza-lap-xuong.jpg',
+    price: '99,000',
+  },
+  {
+    id: 4,
+    title: 'Pizza Rau Củ Thập Cẩm - Veggie Mania',
+    image: '	https://img.dominos.vn/Veggie-mania-Pizza-Rau-Cu-Thap-Cam.jpg',
+    price: '99,000',
+  },
+  {
+    id: 5,
+    title: 'Pizza 4 Vị - Pizza Big 4',
+    image: '	https://img.dominos.vn/Pizza-4-Vi-Pizza-Big-4.jpg',
+    price: '329,000',
+  },
+  {
+    id: 6,
+    title: 'Pizza Hải Sản Kiểu Singapore - Singapore Style Seafood',
+    image:
+      'https://img.dominos.vn/Pizza-Hai-San-Kieu-Singapore-Singapore-Style-Seafood.jpg',
+    price: '249,000',
+  },
+  {
+    id: 7,
+    title: 'Pizza Bò Mê-Hi-Cô Thượng Hạng - Prime Beef',
+    image:
+      'https://img.dominos.vn/Pizza-Bo-Me-Hi-Co-Thuong-Hang-Prime-Beef.jpg',
+    price: '249,000',
+  },
+  {
+    id: 8,
+    title: 'Pizza Gà Phô Mai Thịt Heo Xông Khói - Cheesy Chicken Bacon',
+    image:
+      'https://img.dominos.vn/Pizza-Ga-Pho-Mai-Thit-Heo-Xong-Khoi-Cheesy-Chicken-Bacon.jpg',
+    price: '89,000',
+  },
+]
+
+export default allItems
